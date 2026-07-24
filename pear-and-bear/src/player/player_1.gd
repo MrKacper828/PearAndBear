f9ad9a7c03f6fa4488380e6deb332705cf83441f
MrKacper828 <kapakk.123@gmail.com>
2026-07-24 15:05:27 +0200

    zmiany w kolizjach i dodanie umiejętności rzucania kamieniem

--- pear-and-bear/src/player/player_1.gd
+++ pear-and-bear/src/player/player_1.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -250.0
+const ROCK_SCENE = preload("res://src/player/rock.tscn")
 
+@export var ability_action: String = "ability1"
+
+var facing_direction: float = 1.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +27,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#funkcja zajmująca się ustaleniem kierunku gracza i wywołaniem rzutu
+func _process(delta: float) -> void:
+	if velocity.x > 0:
+		facing_direction = 1.0
+	elif velocity.x < 0:
+		facing_direction = -1.0
+		
+	if Input.is_action_just_pressed("ability1"):
+		throw_rock()
+		
+#logika rzutu kamieniem		
+func throw_rock() -> void:
+	var rock = ROCK_SCENE.instantiate()
+	
+	rock.global_position = global_position
+	var throw_power_x: float = 400.0 * facing_direction
+	var throw_power_y: float = -200.0
+	rock.velocity = Vector2(throw_power_x, throw_power_y)
+	get_tree().root.add_child(rock)
--- pear-and-bear/src/player/player_2.gd
+++ pear-and-bear/src/player/player_2.gd
@@ -4,6 +4,12 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -250.0
 
+const ROCK_SCENE = preload("res://src/player/rock.tscn")
+
+@export var ability_action: String = "ability2"
+
+var facing_direction: float = 1.0
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -22,3 +28,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#funkcja zajmująca się ustaleniem kierunku gracza i wywołaniem rzutu
+func _process(delta: float) -> void:
+	if velocity.x > 0:
+		facing_direction = 1.0
+	elif velocity.x < 0:
+		facing_direction = -1.0
+		
+	if Input.is_action_just_pressed("ability2"):
+		throw_rock()
+
+#logika rzutu kamieniem		
+func throw_rock() -> void:
+	var rock = ROCK_SCENE.instantiate()
+	
+	rock.global_position = global_position
+	var throw_power_x: float = 400.0 * facing_direction
+	var throw_power_y: float = -200.0
+	rock.velocity = Vector2(throw_power_x, throw_power_y)
+	get_tree().root.add_child(rock)
