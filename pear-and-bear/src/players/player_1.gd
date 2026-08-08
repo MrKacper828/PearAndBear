d0b0058fce4bf39786e44cb4883dbc4ced7b7c74
MrKacper828 <kapakk.123@gmail.com>
2026-08-08 20:29:06 +0200

    dodanie coyote time i zmiana graficzna transition gate

--- pear-and-bear/src/players/player_1.gd
+++ pear-and-bear/src/players/player_1.gd
@@ -5,21 +5,24 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -250.0
 const ROCK_SCENE = preload("res://src/players/rock.tscn")
 
+const COYOTE_TIME: float = 0.1
+var coyote_timer: float = 0.0
+
 @export var ability_action: String = "ability1"
 
 var facing_direction: float = 1.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+	else:
+		coyote_timer -= delta
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump1") and is_on_floor():
+	if Input.is_action_just_pressed("jump1") and coyote_timer > 0.0:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		coyote_timer = 0.0
+	
 	var direction := Input.get_axis("move_left1", "move_right1")
 	if direction:
 		velocity.x = direction * SPEED
--- pear-and-bear/src/players/player_2.gd
+++ pear-and-bear/src/players/player_2.gd
@@ -6,21 +6,24 @@ const JUMP_VELOCITY = -250.0
 
 const ROCK_SCENE = preload("res://src/players/rock.tscn")
 
+const COYOTE_TIME: float = 0.1
+var coyote_timer: float = 0.0
+
 @export var ability_action: String = "ability2"
 
 var facing_direction: float = 1.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+	else:
+		coyote_timer -= delta
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump2") and is_on_floor():
+	if Input.is_action_just_pressed("jump2") and coyote_timer > 0.0:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		coyote_timer = 0.0
+	
 	var direction := Input.get_axis("move_left2", "move_right2")
 	if direction:
 		velocity.x = direction * SPEED
