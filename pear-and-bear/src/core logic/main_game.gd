82e8d4084c0157ccf9e46873fb6f1b2a2ea806e0
MrKacper828 <kapakk.123@gmail.com>
2026-08-16 14:17:36 +0200

    naprawienie stutteru kamery przy animacjach i usprawnienie jej działania

--- pear-and-bear/src/core logic/main_game.gd
+++ pear-and-bear/src/core logic/main_game.gd
@@ -14,12 +14,23 @@ extends Node
 @onready var camera_2: Camera2D = $"SplitPlayerCamera/CanvasLayer/HBoxContainer/SubViewportContainer2/SubViewport/Camera2D"
 
 var is_split: bool = false
+var exact_cam1_pos: Vector2
+var exact_cam2_pos: Vector2
 
 func _ready() -> void:
+	#wymuszenie uruchomienia działania kamery dopiero po ruchu fizycznym graczy
+	process_physics_priority = 100
+	
 	#ten sam widok dla viewportu2
 	viewport_2.world_2d = viewport_1.world_2d
 	#merge kamer
 	camera_1.top_level = true
+	
+	#ustawienie kamery na graczach
+	if is_instance_valid(player_1) and is_instance_valid(player_2):
+		exact_cam1_pos = (player_1.global_position + player_2.global_position) / 2.0
+		exact_cam2_pos = player_2.global_position
+		camera_1.global_position = exact_cam1_pos.round()
 	
 	#ukrycie splitu na starcie gry
 	is_split = false
@@ -51,16 +62,16 @@ func _physics_process(_delta: float) -> void:
 		
 	if is_split:
 		#tryb dzielonego ekranu
-		var target_1: Vector2 = camera_1.global_position.lerp(player_1.global_position, lerp_speed)
-		camera_1.global_position = target_1.round()
+		exact_cam1_pos = exact_cam1_pos.lerp(player_1.global_position, lerp_speed)
+		camera_1.global_position = exact_cam1_pos
 		camera_1.zoom = Vector2(1.0, 1.0)
 		
-		var target_2: Vector2 = camera_2.global_position.lerp(player_2.global_position, lerp_speed)
-		camera_2.global_position = target_2.round()
+		exact_cam2_pos = exact_cam2_pos.lerp(player_2.global_position, lerp_speed)
+		camera_2.global_position = exact_cam2_pos
 		camera_2.zoom = Vector2(1.0, 1.0)
 	else:
 		#tryb jednego ekranu
 		var center_pos: Vector2 = (player_1.global_position + player_2.global_position) / 2.0
-		var target_center: Vector2 = camera_1.global_position.lerp(center_pos, lerp_speed)
-		camera_1.global_position = target_center.round()
+		exact_cam1_pos = exact_cam1_pos.lerp(center_pos, lerp_speed)
+		camera_1.global_position = exact_cam1_pos
 		camera_1.zoom = Vector2(1.0, 1.0)
