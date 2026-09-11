478e8b6d4cce7c5cf48481f1bba123e07ab1f446
MrKacper828 <kapakk.123@gmail.com>
2026-09-11 16:16:57 +0200

    wizualizacja trajektorii rzutu kamieni

--- pear-and-bear/src/players/rock_throw_ability.gd
+++ pear-and-bear/src/players/rock_throw_ability.gd
@@ -1,6 +1,8 @@
 extends Node
 
-#zmienne do zmiany dla każdego gracza
+@onready var line_2d: Line2D = $Line2D
+
+#zmienne dla każdego gracza w inspektorze
 @export var rock_scene: PackedScene
 
 @export var max_charge_time: float = 1.0
@@ -41,3 +43,30 @@ func throw_rock(held_time: float, facing_direction: float) -> void:
 	rock.velocity = Vector2(final_power_x, final_power_y)
 	get_parent().get_parent().add_child(rock)
 	
+#linia widoczności trajektorii rzuty
+func _process(_delta: float) -> void:
+	if is_charging:
+		line_2d.visible = true
+		update_trajectory_line()
+	else:
+		line_2d.visible = false
+		
+func update_trajectory_line() -> void:
+	line_2d.clear_points()
+	
+	var start_pos: Vector2 = get_parent().global_position
+	
+	var current_power_x = remap(charge_time, 0.0, max_charge_time, min_power_x, max_power_x) * get_parent().facing_direction
+	var current_power_y = remap(charge_time, 0.0, max_charge_time, min_power_y, max_power_y)
+	
+	var sim_pos: Vector2 = start_pos
+	var sim_vel: Vector2 = Vector2(current_power_x, current_power_y)
+	var gravity: float = 980.0
+	
+	var step_dt: float = 0.05
+	
+	for i in range(25):
+		line_2d.add_point(line_2d.to_local(sim_pos))
+		
+		sim_vel.y += gravity * step_dt
+		sim_pos += sim_vel * step_dt
